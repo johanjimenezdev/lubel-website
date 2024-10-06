95d6265380189279dc4033c93f0a33e2461df877
johanjimenezdev <jimeneza.johan27@gmail.com>
2024-10-05 21:10:27 -0500

    feat: prettier and mui configured

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+    arrowParens: "avoid",
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    semi: false,
+    tabWidth: 2,
+    trailingComma: "none"
+}
